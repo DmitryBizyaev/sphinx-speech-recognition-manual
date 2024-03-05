99319be81aa1d092ec945243a2f94484d23cd6e3
Dmitry Bizyaev <83307719+DmitryBizyaev@users.noreply.github.com>
2024-03-06 01:16:56 +0300

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo $"\napt-get update"
+echo "apt-get update"
 sudo apt-get update &> /dev/null
 
-echo $"\ncheck python version"
+echo "check python version"
 sudo python3 --version 2> /dev/null
 
-echo $"\npip install"
+echo "pip install"
 sudo apt-get install python3-pip -y &> /dev/null
 sudo pip3 --version 2> /dev/null
 
-echo $"\npip update"
+echo "pip update"
 sudo pip3 install --upgrade pip -y &> /dev/null
 sudo pip3 --version 2> /dev/null
 
-echo $"\npocketsphinx install"
+echo "pocketsphinx install"
 sudo pip3 install pocketsphinx &> /dev/null
 sudo pip3 show pocketsphinx 2> /dev/null
 
-echo $"\nserver start"
+echo "server start"
 sudo python3 request_handler.py
